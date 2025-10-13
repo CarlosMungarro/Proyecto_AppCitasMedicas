f88f1176393d9453e405a7ecba2c6e5f6f30c11d
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2025-10-13 06:18:36 -0700

    Mis citas Medico
    Puro diseño sin funcionalidad todavia

---
+++ app/src/main/java/equipocitasmedicas/citasmedicas/ui/MisCitasMedicoActivity.kt
@@ -0,0 +1,18 @@
+package equipocitasmedicas.citasmedicas.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import equipocitasmedicas.citasmedicas.R
+import equipocitasmedicas.citasmedicas.data.CitasStore
+import equipocitasmedicas.citasmedicas.model.CitaItem
+
+class MisCitasMedicoActivity : AppCompatActivity() {
+
+
+
+}
